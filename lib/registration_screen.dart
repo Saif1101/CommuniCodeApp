a313f1bc4569eb54f6c04dc5f0ea264e151797f1
Saif1101 <Saif1101@users.noreply.github.com>
2020-11-20 10:07:28 +0530

    -Added Firebase Authentication Initialization -Added switching between login/registration pages on button press

---
+++ lib/registration_screen.dart
@@ -0,0 +1,16 @@
+import 'package:coding_inventory/utilities/constants.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class registrationPage extends StatefulWidget {
+  @override
+  _registrationPageState createState() => _registrationPageState();
+}
+
+class _registrationPageState extends State<registrationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
